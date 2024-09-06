7ba7df6ccb7113ff5c7a497135f2a4213ef7be70
Martin Mucha <martin.mucha@embedit.cz>
2024-09-06 10:01:48 +0200

    initial
    Signed-off-by: Martin Mucha <martin.mucha@embedit.cz>

---
+++ src/main/java/com/example/is_web_binder_used_to_bind_jackson_dto_request_body/IsWebBinderUsedToBindJacksonDtoRequestBodyApplication.java
@@ -0,0 +1,13 @@
+package com.example.is_web_binder_used_to_bind_jackson_dto_request_body;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsWebBinderUsedToBindJacksonDtoRequestBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsWebBinderUsedToBindJacksonDtoRequestBodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/is_web_binder_used_to_bind_jackson_dto_request_body/IsWebBinderUsedToBindJacksonDtoRequestBodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.is_web_binder_used_to_bind_jackson_dto_request_body;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsWebBinderUsedToBindJacksonDtoRequestBodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
